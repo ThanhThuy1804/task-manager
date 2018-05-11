8212da1d69580c563738a3b2bd9cf8112f8af6fb
ThanhThuy1804 <thuynguyen8243925@gmail.com>
2018-05-11 10:00:20 +0700

    update projects(decription,delete_at), tasks(decription,delete_at) , users(email,password,delete_at) table

--- database/migrations/2018_05_11_015014_create_tasks_table.php
+++ database/migrations/2018_05_11_015014_create_tasks_table.php
@@ -15,15 +15,8 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->integer('project_id')->unsigned();
             $table->timestamps();
         });
-        Schema::table('tasks',function($table){
-            $table->foreign('project_id')->references('id')->on('projects');
-        });
-
-
     }
 
     /**
